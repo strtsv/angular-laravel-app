8cb43d5066c7ab6bfda447aff31f1c1f6c2e8e8e
jsusilo <enr@bk.ru>
2021-01-25 23:46:29 +0500

    Employee model

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/api/v1/employees/{id?}', 'Employees@index');
+Route::post('/api/v1/employees', 'Employees@store');
+Route::post('/api/v1/employees/{id}', 'Employees@update');
+Route::delete('/api/v1/employees/{id}', 'Employees@destroy');
