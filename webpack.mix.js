170bfccf0cdf47e84cfc33e78ee9ad00f992e4e6
jsusilo <enr@bk.ru>
2021-01-29 00:35:18 +0500

    webpack config finish

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,21 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css")
+    .copy(
+        "node_modules/angular/angular.js",
+        "public/js/lib/angular/angular.js"
+    )
+    .copy(
+        "node_modules/jquery/dist/jquery.min.js",
+        "public/js/lib/jquery/jquery.min.js"
+    )
+    .copy(
+        "node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js",
+        "public/js/lib/bootstrap/bootstrap.min.js"
+    )
+    .copy("resources/js/app.js", "public/js/app.js")
+    .copy(
+        "resources/js/controllers/employees.js",
+        "public/js/controllers/employees.js"
+    );
